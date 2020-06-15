a7a188df1e9515e12f7c1771cc2ad5265e40ead8
Lecnier Freeman <lecnier.freeman@tklabs.com>
2020-06-15 17:51:05 -0400

    Update package.swift swift tools version to 5.1

--- Package.swift
+++ Package.swift
@@ -1,4 +1,4 @@
-// swift-tools-version:5.1
+// swift-tools-version:5.2
 // The swift-tools-version declares the minimum version of Swift required to build this package.
 
 import PackageDescription
@@ -6,7 +6,7 @@ import PackageDescription
 let package = Package(
     name: "UIKitPlus",
     platforms: [
-       .macOS(.v10_14), .iOS(.v9),
+       .iOS(.v11),
     ],
     products: [
         // 🏰 Declarative UIKit wrapper inspired by SwiftUI
